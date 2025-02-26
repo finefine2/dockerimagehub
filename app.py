5a1f8c97b131fbef27018a6c21761fc4160ee0d1
Yangho Lee <130417651+finefine2@users.noreply.github.com>
2025-02-26 22:19:22 +0900

    app.py changed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__) 
-@app.route("/") 
-def home(): 
-    return "Hello World" 
+app=Flask(__name__)
 
-if __name__=="__main__": 
+@app.route("/")
+def home():
+    return "Hello World!"
+
+if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
-    
